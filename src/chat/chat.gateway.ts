d828dc8855f357798d6e8136755411d8804c4d26
AhmedKarray1 <karray.ahmed01@gmaiL.com>
2024-04-18 15:32:24 +0100

    start implementing the socket client

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,13 +1,24 @@
 import {
-  SubscribeMessage,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   WebSocketGateway,
-  MessageBody,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() data: any) {
-    console.log(data);
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  sendMessage(message: string) {
+    this.server.emit('message', message);
   }
 }
