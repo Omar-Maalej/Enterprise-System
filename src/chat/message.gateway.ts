beaa0b6ba038e7ed857951436da6650e3979bad4
AhmedKarray1 <karray.ahmed01@gmaiL.com>
2024-04-27 23:05:41 +0100

    fix code

--- src/chat/message.gateway.ts
+++ src/chat/message.gateway.ts
@@ -1,4 +1,10 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Message } from './dto/message.model';
 import { CreateMessageDto } from './dto/create-message.dto';
@@ -8,20 +14,19 @@ import { CreateMessageDto } from './dto/create-message.dto';
     origin: '*',
   },
 })
-
 export class MessagesGateway {
-    /* Permet de récupérer une référence sur socket.io Server afin d'utiliser son api telque le brodcast */
-    @WebSocketServer()
-    server: Server;
-    messges: Message[] = [
-    {id:1, user: 'aymen', content: 'Hello GL3'},
-    {id:2, user: 'GL3', content: 'f54z4f5zf456a??;!!!'},
-    {id:3, user: 'aymen', content: 'Na9sssssou mel 7essss !!!'},
+  /* Permet de récupérer une référence sur socket.io Server afin d'utiliser son api telque le brodcast */
+  @WebSocketServer()
+  server: Server;
+  messges: Message[] = [
+    { id: 1, user: 'aymen', content: 'Hello GL3' },
+    { id: 2, user: 'GL3', content: 'f54z4f5zf456a??;!!!' },
+    { id: 3, user: 'aymen', content: 'Na9sssssou mel 7essss !!!' },
   ];
   @SubscribeMessage('message')
-  handleMessage( ): string {
+  handleMessage(): string {
     console.log('connectiong to message');
-    
+
     return 'Hello world!';
   }
   @SubscribeMessage('allMessages')
@@ -31,12 +36,15 @@ export class MessagesGateway {
   }
 
   @SubscribeMessage('addMessage')
-  addMessage(client: any, @MessageBody() createMessageDto: CreateMessageDto): any {
+  addMessage(
+    client: any,
+    @MessageBody() createMessageDto: CreateMessageDto,
+  ): any {
     const id = this.messges[this.messges.length - 1].id + 1;
-    const {user, content} = createMessageDto;
-    const newMessage = {id, user, content};
+    const { user, content } = createMessageDto;
+    const newMessage = { id, user, content };
     this.messges.push(newMessage);
-    this.server.emit('new-message', newMessage)
+    this.server.emit('new-message', newMessage);
     return this.messges;
   }
 }
