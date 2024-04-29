75edf654549d992e83aa28208039b5bbf9edcbba
samer-mansouri <samer.mansouri@insat.ucar.tn>
2024-04-29 20:20:42 +0100

    Adding swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { config } from 'dotenv';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+
+// async function bootstrap() {
+//   config();
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({origin: true});
+//   await app.listen(3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: true});
+  app.enableCors({ origin: true });
+
+  // Swagger configuration
+  const options = new DocumentBuilder()
+    .setTitle('App documentation')
+    .setDescription('This is the documentation of the app APIs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -3,7 +3,9 @@ import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { Message } from './entities/message.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('messages')
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
