d5c693048f4d570450f46cf575201479a7619782
Ahmed Karray <118677561+AhmedKarray1@users.noreply.github.com>
2024-05-26 17:14:21 +0000

    Defining the comment entity

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,7 +1,19 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Post } from 'src/posts/entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @ObjectType()
 export class Comment {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int, { description: 'Unique identifier for the comment' })
+  id: number;
+
+  @Field(() => String, { description: 'Content of the comment' })  
+  content: string;
+
+  @Field(() => Int, { description: 'Unique identifier for the post' })
+  author: User;
+
+  @Field(() => Int, { description: 'Unique identifier for the user' })
+  post: Post;
+
 }
