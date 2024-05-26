d517151f3bdd84e6f94c06cbcdebd77a99f52303
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-05-26 11:09:13 +0100

    Merge branch 'room-rest' into 'main'

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -3,7 +3,7 @@ import { RoomService } from './room.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 
-@Controller('room')
+@Controller('rooms')
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
