d8d3066272bd0aec35046e1c606b0595e5dcb5cb
Lost Elf <ilef.chebil@insat.ucar.tn>
2024-05-26 22:59:09 +0000

    feat: jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,51 +1,75 @@
-import {Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { UserLoginDto } from './dto/user-login.dto';
 import { User } from 'src/users/entities/user.entity';
-
+import { UserRegisterDto } from './dto/user-register.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-        private jwtService: JwtService
-      ) {}
-      
-    
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private jwtService: JwtService
+  ) {}
 
+  async register(registerAuthDto: UserRegisterDto): Promise<void> {
+    const { username, password, email } = registerAuthDto;
 
-      async login(credentials: UserLoginDto) {
-        const { username, password } = credentials;
-        // We can log in via username or email
-        const user = await this.userRepository.createQueryBuilder("user")
-          .where("user.username = :username or user.email = :username",{ username: username })
-          .getOne();
-        console.log(username);
-        if (!user)
-          throw new NotFoundException('Wrong username or password');
-      
-        // Check password if user exists
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (isPasswordValid) {
-            const payload = {
-                username: user.username,
-                email: user.email,
-                role: user.role
-              };
-              const jwt = await this.jwtService.sign(payload);
-              return {
-                "access_token" : jwt
-              };
-        } else {
-          // If password is wrong
-          throw new NotFoundException('Wrong username or password');
-        }
+    const user = new User();
+    user.username = username;
+    user.email = email;
+    user.salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(password, user.salt);
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already taken');
+      } else {
+        throw new InternalServerErrorException();
       }
-      private async hashPassword(password: string, salt: string): Promise<string> {
-        return bcrypt.hash(password, salt);
-      }
- }
+    }
+  }
+ 
+
+  async login(credentials: UserLoginDto) {
+    const { username, password } = credentials;
+
+    const user = await this.userRepository.createQueryBuilder("user")
+      .where("user.username = :username or user.email = :username", { username })
+      .getOne();
+
+    if (!user) {
+      throw new NotFoundException('Wrong username or password');
+    }
+
+    // Logging for debugging
+    console.log('Entered username:', username);
+    console.log('Entered password:', password);
+    console.log('Stored hashed password:', user.password);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log('Password valid:', isPasswordValid);
+
+    if (isPasswordValid) {
+      const payload = {
+        username: user.username,
+        email: user.email,
+        role: user.role
+      };
+
+      console.log('JWT payload:', payload);
+
+      const jwt = await this.jwtService.sign(payload);
+      return {
+        access_token: jwt
+      };
+    } else {
+      throw new NotFoundException('Wrong username or password');
+    }
+  }
+}
