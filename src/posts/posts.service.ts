bb6c7fbcea5eb923dfec041f2408f3a5891c8df9
Ahmed Karray <118677561+AhmedKarray1@users.noreply.github.com>
2024-05-26 18:00:26 +0000

    Adding posts service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
-  create(createPostInput: CreatePostInput) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  create(createPostInput: CreatePostInput): Promise<Post> {
+    const newPost = this.postsRepository.create(createPostInput);
+    return this.postsRepository.save(newPost);
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  findAll(): Promise<Post[]> {
+    return this.postsRepository.find({ relations: ['author', 'comments'] });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number): Promise<Post> {
+    const post = await this.postsRepository.findOne({ where: { id }, relations: ['author', 'comments'] });
+    if (!post) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+    return post;
   }
 
-  update(id: number, updatePostInput: UpdatePostInput) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostInput: UpdatePostInput): Promise<Post> {
+    const post = await this.findOne(id);
+    Object.assign(post, updatePostInput);
+    return this.postsRepository.save(post);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number): Promise<Post> {
+    const post = await this.findOne(id);
+    return this.postsRepository.remove(post);
   }
 }
