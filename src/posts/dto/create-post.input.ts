1aabc45eb0707271c0d0bb1050938d34050ca150
Ahmed Karray <118677561+AhmedKarray1@users.noreply.github.com>
2024-05-26 17:26:47 +0000

    Adding create post dto

--- src/posts/dto/create-post.input.ts
+++ src/posts/dto/create-post.input.ts
@@ -2,6 +2,9 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreatePostInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Content of the post' })
+  content: string;
+
+  @Field(() => Int, { description: 'Unique identifier for the author (user)' })
+  authorId: number;
 }
