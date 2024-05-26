7f0837c5c0edca72a48e5bce399d5562b6162b37
samer-mansouri <samer.mansouri@insat.ucar.tn>
2024-05-26 19:41:20 +0100

    Fixing some import errors

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsResolver } from './comments.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { PostsModule } from 'src/posts/posts.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Comment]),
+    PostsModule,
+    UsersModule,
+  ],
   providers: [CommentsResolver, CommentsService],
 })
 export class CommentsModule {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsService } from './posts.service';
 import { PostsResolver } from './posts.resolver';
+import { Post } from './entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Comment } from 'src/comments/entities/comment.entity';
 
 @Module({
-  providers: [PostsResolver, PostsService],
+  imports: [TypeOrmModule.forFeature([Post, User, Comment])],
+  providers: [PostsService, PostsResolver],
+  exports: [PostsService, TypeOrmModule], // Export TypeOrmModule to make repositories available
 })
 export class PostsModule {}
