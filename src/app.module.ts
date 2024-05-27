694cc142d0463bebacc4cf7e1319c0c7a662f034
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-05-27 04:24:23 +0100

    merging to main from user-roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ dotenv.config();
       autoSchemaFile: 'src/schema.gql',
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: 'mysql',
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USER,
