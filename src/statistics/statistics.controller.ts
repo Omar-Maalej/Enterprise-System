597ff2b283294d1df6ee2396c9aec7ea309a351e
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-05-27 20:11:05 +0100

    feat: statistics endpoint

---
+++ src/statistics/statistics.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { StatisticsService } from './statistics.service';
+
+@Controller('statistics')
+export class StatisticsController {
+  constructor(private readonly statisticsService: StatisticsService) {}
+
+  @Get()
+  async getStatistics() {
+    return this.statisticsService.getStatistics();
+  }
+}
