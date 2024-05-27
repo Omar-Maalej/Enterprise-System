ec7197837f241539b33ea8333dc57c72bbe4f68b
samer-mansouri <samer.mansouri@insat.ucar.tn>
2024-05-27 08:07:42 +0100

    Fixing services dependency bugs

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,3 +1,4 @@
+// users.module.ts
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
@@ -5,11 +6,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { SseModule } from 'src/sse/sse.module';
 import { JwtModule } from '@nestjs/jwt';
+import { RedisModule } from 'src/redis/redis.module';
+import { RedisService } from 'src/redis/redis.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), SseModule, JwtModule],
+  imports: [TypeOrmModule.forFeature([User]), SseModule, JwtModule, RedisModule],
   controllers: [UsersController],
-  providers: [UsersService],
-  exports : [TypeOrmModule.forFeature([User]), UsersService]  
+  providers: [UsersService, RedisService],
+  exports: [TypeOrmModule.forFeature([User]), UsersService],
 })
 export class UsersModule {}
